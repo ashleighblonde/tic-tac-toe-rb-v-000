f84efff360baf8bed2c9d813afa659fdf008d8ac
Ashleigh Petty <petty.ashleigh@yahoo.com>
2020-05-23 20:35:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,7 +105,8 @@ def turn(board)
    index = input_to_index(user_input)
    if valid_move?(board, index)
       move(board, index, current_player(board))
-      turn(board)
+      else 
+        turn(board)
    end
    display_board(board)
 end
