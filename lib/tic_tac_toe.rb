4400b8e1db6abdd60b8a356d1307992851279959
Ashleigh Petty <petty.ashleigh@yahoo.com>
2020-05-23 20:30:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,7 +117,7 @@ def play(board)
       winner(board) == "X" || winner(board) == "O"
       puts "Congratulations #{winner(board)}!"
    elsif draw?(board)
-      
+      puts "It's draw!"
    end
 end
 
